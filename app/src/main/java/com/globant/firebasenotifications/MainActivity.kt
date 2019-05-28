07e57dad53691e72ea109914cef123062a2022cd
Francisco Prieto <francisco.prieto@globant.com>
2019-05-28 10:37:06 -0300

    Deleted subscription to topics, as it was unnecesary

--- app/src/main/java/com/globant/firebasenotifications/MainActivity.kt
+++ app/src/main/java/com/globant/firebasenotifications/MainActivity.kt
@@ -1,40 +1,12 @@
 package com.globant.firebasenotifications
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create channel to show notifications.
-            val channelId = "base"
-            val channelName = "base"
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager?.createNotificationChannel(
-                NotificationChannel(channelId,
-                    channelName, NotificationManager.IMPORTANCE_LOW)
-            )
-        }
-
-
-        FirebaseMessaging.getInstance().subscribeToTopic("base")
-            .addOnCompleteListener { task ->
-                var msg = getString(R.string.msg_subscribed)
-                if (!task.isSuccessful) {
-                    msg = getString(R.string.msg_subscribe_failed)
-                }
-                Log.d("Base topic subscription", msg)
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-            }
     }
 }
