31efc0ab0cd0812a0680fa56a47110ef60146215
Francisco Prieto <francisco.prieto@globant.com>
2019-05-31 15:01:16 -0300

    Appcenter test

--- app/src/main/java/com/globant/firebasenotifications/MainActivity.kt
+++ app/src/main/java/com/globant/firebasenotifications/MainActivity.kt
@@ -2,11 +2,15 @@ package com.globant.firebasenotifications
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(application, "0cd642fc-5b22-4b3b-9ac9-cfaad7cfa9d4", Analytics::class.java, Crashes::class.java)
     }
 }
